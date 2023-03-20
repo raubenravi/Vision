5b6739762a2d7d3cf49466e2566423c01b275b8b
Ruben Rajan <rubenrara@gmail.com>
2023-03-20 16:53:00 +0100

    veel geklooied aan nueuralen netwerken

--- code_eind_opdracht/Preprocess.py
+++ code_eind_opdracht/Preprocess.py
@@ -7,20 +7,23 @@ from sklearn.utils import shuffle
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin
 import tensorflow as tf
+from skimage import io, filters
+
 
 def recreate_image(codebook, labels, w, h):
     """Recreate the (compressed) image from the code book & labels"""
     return codebook[labels].reshape(w, h, -1)
 
-def FilterImage(img = None, grayscale = True, Relu = True, Kmean=True, n_colors = 50):
+def FilterImage(img = None, grayscale = True, Relu = True, Kmean=True, n_colors = 200):
     grayscale = rgb2gray(img)
+    grayscale = filters.butterworth(grayscale)
 
     grayscale = np.array(grayscale, dtype=np.float64) / 255
     w, h = original_shape = tuple(grayscale.shape)
     image_array = np.reshape(grayscale, (w * h, 1))
 
     image_array_sample = shuffle(image_array, random_state=0, n_samples=1_000)
-    kmeans = KMeans(n_clusters=5, n_init="auto", random_state=0).fit(
+    kmeans = KMeans(n_clusters=125, n_init="auto", random_state=0).fit(
         image_array_sample
     )
 
@@ -31,11 +34,19 @@ def FilterImage(img = None, grayscale = True, Relu = True, Kmean=True, n_colors 
     labels_random = pairwise_distances_argmin(codebook_random, image_array, axis=0)
 
 
-    layer = tf.keras.layers.ReLU(max_value=4.0)
+    layer = tf.keras.layers.ReLU(max_value=100.0)
     # kmeans.cluster_centers_
     #Relu = layer(labels_random)
     #Relu_array = np.reshape(Relu, (w * h, 1))
-    #Relu = recreate_image(Relu_array, labels, w, h)
-    Relu = recreate_image(codebook_random, labels, w, h)
+    Relu = recreate_image(codebook_random, labels_random, w, h)
+
     return Relu
 
+i = 1
+##load image
+img_path = "./data/Image/{}.jpg".format(i)
+print(img_path)
+image = io.imread(img_path)[:,:,:3]
+image = FilterImage(img=image)
+shower = skimage.io.imshow(image, cmap=plt.cm.gray)
+plt.show()
--- code_eind_opdracht/backup.py
+++ code_eind_opdracht/backup.py
@@ -1,4 +1,66 @@
-from tensorflow_datasets.image_classification.colorectal_histology_test import num_classes
+import matplotlib.pyplot as plt
+import numpy as np
+import skimage.io
+from skimage.color import rgb2hsv, rgb2gray
+import skimage.io as io
+from sklearn.utils import shuffle
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+import tensorflow as tf
+
+def recreate_image(codebook, labels, w, h):
+    """Recreate the (compressed) image from the code book & labels"""
+    return codebook[labels].reshape(w, h, -1)
+
+def FilterImage(img = None, grayscale = True, Relu = True, Kmean=True, n_colors = 10):
+    grayscale = rgb2gray(img)
+
+    grayscale = np.array(grayscale, dtype=np.float64) / 255
+    w, h ,d= original_shape = tuple(img.shape)
+    image_array = np.reshape(img, (w * h, d))
+
+    image_array_sample = shuffle(image_array, random_state=0, n_samples=1_000)
+    kmeans = KMeans(n_clusters=5, n_init="auto", random_state=0).fit(
+        image_array_sample
+    )
+
+    labels = kmeans.predict(image_array)
+
+    codebook_random = shuffle(image_array, random_state=0, n_samples=5)
+
+    labels_random = pairwise_distances_argmin(codebook_random, image_array, axis=0)
+
+
+    layer = tf.keras.layers.ReLU(max_value=100.0)
+    # kmeans.cluster_centers_
+    #Relu = layer(labels_random)
+    #Relu_array = np.reshape(Relu, (w * h, 1))
+    Relu = recreate_image(codebook_random, labels_random, w, h)
+    return rgb2gray(Relu)
+
+i = 1
+# load image
+img_path = "./data/Image/{}.jpg".format(i)
+print(img_path)
+image = io.imread(img_path)[:,:,:3]
+image = FilterImage(img=image)
+shower = skimage.io.imshow(image, cmap=plt.cm.gray)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from tensorflow_datasets.object_detection.open_images_challenge2019_beam import cv2
 
 from Preprocess import FilterImage, recreate_image
@@ -15,7 +77,7 @@ from skimage.transform import rescale, resize, downscale_local_mean
 from tensorflow_examples.profiling.resnet_model import layers
 patches = []
 labels = []
-for i in range(3) :
+for i in range(5) :
     #i = 1
     # load image
     img_path = "./data/Image/{}.jpg".format(i)
@@ -53,7 +115,7 @@ for i in range(3) :
         for i in range(0, image.shape[0], patch_size[0]):
             for j in range(0, image.shape[1], patch_size[1]):
                 patch = image[i:i+patch_size[0], j:j+patch_size[1]]
-                if (patch.shape != (patch_size[0], patch_size[1], 1)):
+                if (patch.shape != (32, 32, 1)):
                     continue
                # patch = resize(patch, (32*32 , 1))
                 #print(patch.shape)
@@ -73,51 +135,45 @@ labels = np.array(labels)
 # Train the model on the patches and labels
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 model = tf.keras.models.Sequential([
-             tf.keras.Input(shape=(32,32,1)),
-             layers.Conv2D(10,kernel_size=(16, 16)),
-             layers.Conv2D(10, kernel_size=(8, 8)),
-             layers.MaxPooling2D(pool_size=(3, 3)),
-             layers.MaxPooling2D(pool_size=(3, 3)),
-            tf.keras.layers.Flatten()
-         ])
-#louter convelouties en dan pooling
-#convevelutie padding valid  -> 5*5  (2 keer) daarna 2 keer pooling
+  tf.keras.layers.Conv2D(5, 5, input_shape=(32,32, 1),  activation='relu'),
+    tf.keras.layers.Dense(30),
+    tf.keras.layers.Conv2D(5, 5, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(10),
+])
+
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(patches, labels, epochs=1)
-
-model.save('saved_model/my_model')
-#eroderen
-#neuraalNetwerk
-#neurnetwerk met convoluties
+model.fit(patches, labels, epochs=5)
 
 
 
-i = 3091
+
+
+
+i = 3018
 # load image
 img_path = "./data/Image/{}.jpg".format(i)
 image = io.imread(img_path)[:,:,:3]
-image = resize(image, (100,100))
+image = resize(image, (250,250))
 original = image
 image = FilterImage(image)
-plt.imshow(image)
-plt.show()
 #image = tf.expand_dims(image, 0)
 print(image.shape)
-
 patches = []
-for i in range(0, image.shape[0]-patch_size[0]):
-    for j in range(0, image.shape[1]-patch_size[1]):
+for i in range(0, image.shape[0]-32):
+    for j in range(0, image.shape[1]-32):
         patch = image[i:i+patch_size[0], j:j+patch_size[1]]
-        if (patch.shape != (int(patch_size[0]), int(patch_size[1]), 1)):
-            patch = np.zeros((int(patch_size[0]), int(patch_size[1]), 1))
+        if (patch.shape != (32, 32, 1)):
+            patch = np.zeros((32, 32, 1))
             #print(i,j)
             #continue
         #patch = resize(patch, (32*32 , 1))
-        patch = tf.image.resize(patch, (  int(patch_size[0]) , int(patch_size[1])) )
+        patch = tf.image.resize(patch, (32 , 32))
         patches.append(patch)
+
 
 patches = np.array(patches)
 preds = model.predict(patches)
